a0c72b3906a3a10dc5584a116370f0b2e5b48b48
Loukas Mironidis <lmironidis@worcester.edu>
2021-08-27 17:09:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
-import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -9,53 +8,34 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div style={{padding: "10rem"}}>
+        <div style={{ padding: "10rem" }}>
           <p className="lead py-5 display-3">Hello World, ipman is back!</p>
 
           <Form
-            name="contact-v1"
+            name="contact v1"
             method="post"
             data-netlify="true"
             onSubmit="submit">
 
-            <Form.Group className="mb-3" controlId="contact-v1.NameInput">
-              <FloatingLabel controlId="floatingInput" label="Your Name" className="mb-3">
-                <Form.Control
-                  required
-                  name="name"
-                  type="text"
-                  placeholder="Normal text" />
-                <Form.Control.Feedback type="invalid">
-                  Please enter your name.
-                </Form.Control.Feedback>
-              </FloatingLabel>
+            <Form.Group>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                required
+                type="text"/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="contact-v1.EmailInput">
-              <FloatingLabel controlId="floatingInput" label="Email address" className="mb-3">
-                <Form.Control
-                  required
-                  name="email"
-                  type="email"
-                  placeholder="name@example.com" />
-                <Form.Control.Feedback type="invalid">
-                  Please enter valid email address like (name@example.com).
-                </Form.Control.Feedback>
-              </FloatingLabel>
+            <Form.Group>
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                required
+                type="text"/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="contact-v1.MessageInput">
-              <FloatingLabel controlId="floatingTextarea" label="Message">
-                <Form.Control
-                  required
-                  name="message"
-                  as="textarea"
-                  placeholder="Leave a message here"
-                  style={{ height: '150px' }} />
-                <Form.Control.Feedback type="invalid">
-                  Please enter a message.
-                </Form.Control.Feedback>
-              </FloatingLabel>
+            <Form.Group>
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                required
+                type="textarea"/>
             </Form.Group>
 
             <Button variant="primary" type="submit">
