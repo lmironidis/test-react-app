905a3ab57a79080921d074b2a1bdd85b3c85b114
Loukas Mironidis <lmironidis@worcester.edu>
2021-08-27 16:57:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import { Button } from "react-bootstrap";
